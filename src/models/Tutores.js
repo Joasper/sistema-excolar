b95506131de9ae6aab69011df480f4c54de152a0
Joasper <joasper1306@gmail.com>
2023-11-26 15:31:07 -0400

    Creacion de la entidad tutores

---
+++ src/models/Tutores.js
@@ -0,0 +1,39 @@
+import { Schema, model, models } from "mongoose";
+
+const TutoresSchema = new Schema({
+  Estudiante: {
+    type: Schema.Types.ObjectId,
+    ref: "Estudiante",
+    required: true,
+  },
+  Cedula: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 11,
+    unique: true,
+  },
+  Nombre: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  Apellido: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  Telefono: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 10,
+    maxlength: 10,
+  },
+});
+
+export default models.Tutores || model("Tutores", TutoresSchema);
